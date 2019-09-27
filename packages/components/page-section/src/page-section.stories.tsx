6c655aac02415098f6126eb8aff0095671b0f579
Kevin Reynolds <kevin.reynolds@nimbletank.com>
2019-09-27 16:00:11 +0100

    feat(storybook): reset default (#12)

--- packages/components/page-section/src/page-section.stories.tsx
+++ packages/components/page-section/src/page-section.stories.tsx
@@ -17,7 +17,7 @@ storiesOf('Components|page-section', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add('Default', () => (
     <PageSection
-      style={select('Style', PageSectionTypes, PageSectionStyle.PrimaryPattern)}
+      style={select('Style', PageSectionTypes, PageSectionStyle.Default)}
       bgImage={files('Background Image (for Image style)', '.jpg, .svg, .gif, .png')}
     >
       {text('Content', 'This is my content')}

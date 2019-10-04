f35b6d45a2b46e1df055b2428e805cdce32293f3
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2019-10-04 17:56:12 +0100

    hold

---
+++ packages/components/menu/src/menu.component.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+// @ts-ignore
+import * as styles from './menu.module.css';
+
+export interface IMenuItem {
+  label: string;
+  link: string;
+}
+
+export interface IMenuProps {
+  links: IMenuItem[];
+}
+
+const Menu: React.FC<IMenuProps> = ({ links }) => {
+  return (
+    <div className={styles.nav}>
+      <ul>
+        {links &&
+          links.map(({ label, link }, count) => {
+            return (
+              <li key={`footer-menu-${count}`}>
+                <a href={link}>{label}</a>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;

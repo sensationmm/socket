be9dbb451a15e249dcb17a3116088152bbde08e0
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2019-10-16 15:46:21 +0100

    feat(repo): add knobs (DA_UK-344)

--- packages/components/_form/select/src/select.stories.tsx
+++ packages/components/_form/select/src/select.stories.tsx
@@ -1,15 +1,19 @@
 import { action } from '@storybook/addon-actions';
-import { withKnobs } from '@storybook/addon-knobs';
+import { object, select, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import Select, { FormSelectType } from '.';
 
+const types = {
+  Inline: FormSelectType.Inline,
+};
+
 const selectProps = {
   label: '',
-  type: FormSelectType.Inline,
+  type: select('Type', types, types.Inline),
   onChange: action('onChange'),
-  options: [{ val: 'opt1', label: 'option 1' }, { val: 'opt2', label: 'option 2' }],
+  options: object('Options', [{ val: 'opt1', label: 'option 1' }, { val: 'opt2', label: 'option 2' }]),
   defaultOptionText: 'Select option',
   error: '',
   value: '',

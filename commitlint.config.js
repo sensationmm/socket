88ae2d037b020b1956f0eb9b0adab69b34957afc
Tom Maton <thomas.maton@somoglobal.com>
2019-10-16 14:09:05 +0100

    feat(netlify): integration of netlify
    DA_UK-447

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,33 @@
-const { getPackages } = require('@commitlint/config-lerna-scopes').utils;
+const { getPackages } = require("@commitlint/config-lerna-scopes").utils;
 
-const customScopes = ['repo', 'deps', 'storybook', 'jest', 'workflow'];
+const customScopes = [
+  "repo",
+  "deps",
+  "storybook",
+  "jest",
+  "netlify",
+  "terraform",
+  "codepipelines",
+  "workflow"
+];
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   parserPreset: {
     parserOpts: {
-      issuePrefixes: ['#', 'DA_UK-', 'PODD-'],
-    },
+      issuePrefixes: ["#", "DA_UK-", "PODD-"]
+    }
   },
   rules: {
-    'scope-empty': [2, 'never'],
-    'scope-enum': (ctx) =>
-      getPackages(ctx).then((packages) => {
-        const pkgs = packages.map((name) => name.replace(/^pda-([a-z0-9]*)-([a-z0-9-]*)$/gi, '$2')).sort();
-        return [2, 'always', [...customScopes, ...pkgs, 'azp']];
+    "scope-empty": [2, "never"],
+    "scope-enum": ctx =>
+      getPackages(ctx).then(packages => {
+        const pkgs = packages
+          .map(name => name.replace(/^pda-([a-z0-9]*)-([a-z0-9-]*)$/gi, "$2"))
+          .sort();
+        return [2, "always", [...customScopes, ...pkgs, "azp"]];
       }),
-    'references-empty': [2, 'never'],
-    'header-max-length': [0, 'never', 200],
-  },
+    "references-empty": [2, "never"],
+    "header-max-length": [0, "never", 200]
+  }
 };

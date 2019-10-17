e60ff0086812556f5944326a72ed1677b56b6091
Ionut Cirja <ionut.cirja@gmail.com>
2019-10-17 14:53:14 +0100

    feat(article-author): create article author component (DA_UK-382)

---
+++ packages/components/article-author/src/article-author.component.tsx
@@ -0,0 +1,39 @@
+import cx from 'classnames';
+import * as React from 'react';
+
+import Avatar from '@somo/oxd-components-avatar/src';
+import Text, { ColorStyles, TextStyles } from '@somo/pda-components-text/src';
+import { formatBlogPostISODate } from '@somo/pda-utils-dates/src';
+
+import * as styles from './article-author.module.css';
+
+export interface IArticleAuthorProps {
+  name: string;
+  avatar?: string;
+  isAvatarSmall?: boolean;
+  date?: string;
+}
+
+const ArticleAuthor: React.FC<IArticleAuthorProps> = ({ name, avatar, isAvatarSmall, date }) => (
+  <div className={cx(styles.articleAuthor, { [styles.small]: isAvatarSmall })}>
+    <div>
+      <Avatar picture={avatar} alt={name} isSmall={isAvatarSmall} />
+    </div>
+    <div>
+      <Text className={styles.name} color={ColorStyles.secondary} type={TextStyles.body}>
+        {name}
+      </Text>
+      {date && (
+        <Text className={styles.date} color={ColorStyles.secondary} type={TextStyles.caption}>
+          {formatBlogPostISODate(date)}
+        </Text>
+      )}
+    </div>
+  </div>
+);
+
+ArticleAuthor.defaultProps = {
+  isAvatarSmall: false,
+};
+
+export default ArticleAuthor;

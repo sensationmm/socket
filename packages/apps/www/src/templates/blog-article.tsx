6bae630b7c9f96ef95c3a4bdd9587fd7003d8b91
Tom Maton <thomas.maton@somoglobal.com>
2019-10-21 13:50:39 +0100

    feat(netlify): update to graphql query
    DA_UK-447

--- packages/apps/www/src/templates/blog-article.tsx
+++ packages/apps/www/src/templates/blog-article.tsx
@@ -42,7 +42,7 @@ export const query = graphql`
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
+      html
       frontmatter {
         author {
           frontmatter {

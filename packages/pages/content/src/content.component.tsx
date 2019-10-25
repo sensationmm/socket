3277d4a50fe9983a1853d72b9f4544fa86490644
ralphwengerek <ralph.wengerek@gmail.com>
2019-10-25 10:26:41 +0100

    feat(content): added content page (DA_UK-380)

---
+++ packages/pages/content/src/content.component.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import PageSection from '@somo/pda-components-page-section/src';
+import Text, { ColorStyles, TextStyles } from '@somo/pda-components-text/src';
+import RegularLayout from '@somo/pda-layouts-regular/src';
+
+import * as styles from './content.module.css';
+
+interface IContentProps {
+  hero: {
+    title: string;
+  };
+  subTitle: string;
+  body: string;
+  i18n: Pick<EON.IWebAppTranslations['site'], 'footer'>;
+}
+
+const ContentPage: React.FC<IContentProps> = ({ i18n, hero, subTitle, body }) => {
+  const { footer } = i18n;
+
+  return (
+    <RegularLayout hero={hero} footer={footer}>
+      <PageSection>
+        <Text element="h3" type={TextStyles.h3} className={styles.contentPageSubTitle}>
+          {subTitle}
+        </Text>
+        {body.split('\n\n').map((paragraph, count) => (
+          <Text key={`text-${count}`} element={'p'} type={TextStyles.body} color={ColorStyles.tertiary}>
+            {paragraph}
+          </Text>
+        ))}
+      </PageSection>
+    </RegularLayout>
+  );
+};
+
+export default ContentPage;
---
+++ packages/pages/content/src/index.ts
@@ -0,0 +1,3 @@
+import Component from './content.component';
+
+export default Component;

6e06e3b53c338ade14261b980454ae69ab39e69c
ralphwengerek <ralph.wengerek@gmail.com>
2019-10-28 11:29:48 +0000

    feat(content): add storybook knobs (DA_UK-380)

--- packages/pages/content/src/content.stories.tsx
+++ packages/pages/content/src/content.stories.tsx
@@ -1,25 +1,31 @@
+import { text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import Component from './content.component';
 
-const props = {
+const contentPageKnobs = () => ({
   hero: {
-    title: 'Default Template',
+    title: text('Hero title', 'Default Template'),
+    subTitle: text('Heru subtitle', ''),
+    cta: text('CTA', ''),
   },
-  subTitle: 'Cum sociis natoque penatibus et magnis dis.',
-  body: `Purus gravida quis blandit turpis cursus in hac habitasse. Dui ut ornare lectus sit amet est.
-        Tincidunt lobortis feugiat vivamus at augue. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. 
-        Non blandit massa enim nec dui nunc mattis. Vestibulum lectus mauris ultrices eros in cursus turpis massa.\n\n 
-        Sit amet consectetur adipiscing elit ut aliquam purus sit. In nisl nisi scelerisque eu. Eget duis at tellus at 
-        urna condimentum mattis pellentesque id. Ut placerat orci nulla pellentesque dignissim. Nulla facilisi nullam 
-        vehicula ipsum a arcu. Purus non enim praesent elementum facilisis. Et tortor at risus viverra adipiscing at in tellus. 
-        Vel pharetra vel turpis nunc eget. Dui vivamus arcu felis bibendum ut tristique et egestas quis.\n\n
-        Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Fringilla phasellus faucibus scelerisque eleifend. 
-        Vivamus arcu felis bibendum ut tristique et egestas quis. Orci a scelerisque purus semper eget. 
-        Ullamcorper a lacus vestibulum sed arcu non odio. Lobortis mattis aliquam faucibus purus in massa. 
-        Amet luctus venenatis lectus magna fringilla urna porttitor. Erat velit scelerisque in dictum non 
-        consectetur a erat nam. Leo integer malesuada nunc vel.`,
+  subTitle: text('Subtitle', 'Cum sociis natoque penatibus et magnis dis.'),
+  body: text(
+    'Content body',
+    `Purus gravida quis blandit turpis cursus in hac habitasse. Dui ut ornare lectus sit amet est.
+    Tincidunt lobortis feugiat vivamus at augue. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. 
+    Non blandit massa enim nec dui nunc mattis. Vestibulum lectus mauris ultrices eros in cursus turpis massa.\n\n 
+    Sit amet consectetur adipiscing elit ut aliquam purus sit. In nisl nisi scelerisque eu. Eget duis at tellus at 
+    urna condimentum mattis pellentesque id. Ut placerat orci nulla pellentesque dignissim. Nulla facilisi nullam 
+    vehicula ipsum a arcu. Purus non enim praesent elementum facilisis. Et tortor at risus viverra adipiscing at in tellus. 
+    Vel pharetra vel turpis nunc eget. Dui vivamus arcu felis bibendum ut tristique et egestas quis.\n\n
+    Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Fringilla phasellus faucibus scelerisque eleifend. 
+    Vivamus arcu felis bibendum ut tristique et egestas quis. Orci a scelerisque purus semper eget. 
+    Ullamcorper a lacus vestibulum sed arcu non odio. Lobortis mattis aliquam faucibus purus in massa. 
+    Amet luctus venenatis lectus magna fringilla urna porttitor. Erat velit scelerisque in dictum non 
+    consectetur a erat nam. Leo integer malesuada nunc vel.`,
+  ),
   i18n: {
     footer: {
       title: 'Our smart technology needs a smart meter.',
@@ -27,8 +33,8 @@ const props = {
       copyright: '2019 © Socket Energy. All rights reserved.',
     },
   },
-};
-
-storiesOf('Pages|content', module).add('default', () => {
-  return <Component {...props} />;
 });
+
+storiesOf('Pages|content', module)
+  .addDecorator(withKnobs)
+  .add('default', () => <Component {...contentPageKnobs()} />);

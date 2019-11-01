55e32559cabaf796889fc2c1cd655b6b6087cdb4
Tom Maton <thomas.maton@somoglobal.com>
2019-11-01 20:27:16 +0000

    fix(www): fix to conflicts
    DA_UK-693

--- packages/apps/www/src/utils/html-serializer.tsx
+++ packages/apps/www/src/utils/html-serializer.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 
-// import Image from '@somo/pda-components-image/src';
+import Image from '@somo/pda-components-image/src';
 import List from '@somo/pda-components-list/src';
 import Text, { ColorStyles, TextStyles } from '@somo/pda-components-text/src';
-
-// -- Function to add unique key to props
-// const propsWithUniqueKey = (props, key) => Object.assign(props || {}, { key });
 
 const listContent = (content) => content.map((listItem) => listItem.children[0].children[0].value);
 
 const htmlSerializer = (content) => {
-  //   content.children.forEach((comp) => {
-  //     return componentFormatter(comp);
-  //   });
   return content.children.map((comp) => componentFormatter(comp));
 };
 
@@ -28,15 +22,6 @@ const componentFormatter = (component) => {
         </Text>
       );
 
-    // case Elements.heading4: // Heading 4
-    //   return React.createElement('h4', propsWithUniqueKey(props, key), children);
-
-    // case Elements.heading5: // Heading 5
-    //   return React.createElement('h5', propsWithUniqueKey(props, key), children);
-
-    // case Elements.heading6: // Heading 6
-    //   return React.createElement('h6', propsWithUniqueKey(props, key), children);
-
     case 'paragraph': // Paragraph
       if (component.children[0].type === 'text') {
         return (
@@ -48,82 +33,8 @@ const componentFormatter = (component) => {
         return <Image alt={component.children[0].alt || ''} src={component.children[0].url} isLazy={true} />;
       }
 
-    // case Elements.preformatted: // Preformatted
-    //   return React.createElement('pre', propsWithUniqueKey(props, key), children);
-
-    // case Elements.strong: // Strong
-    //   return React.createElement('strong', propsWithUniqueKey(props, key), children);
-
-    // case Elements.em: // Emphasis
-    //   return React.createElement('em', propsWithUniqueKey(props, key), children);
-
-    // case Elements.listItem: // Unordered List Item
-    //   return React.createElement('li', propsWithUniqueKey(props, key), children);
-
-    // case Elements.oListItem: // Ordered List Item
-    //   return React.createElement('li', propsWithUniqueKey(props, key), children);
-
     case 'list': // Unordered List
       return <List listContent={listContent(component.children)} textColor={ColorStyles.tertiary} />;
-
-    // case Elements.oList: // Ordered List
-    //   return React.createElement('ol', propsWithUniqueKey(props, key), children);
-
-    // case 'image': // Image
-    //   return <Image alt={component.children[0].alt || ''} src={component.children[0].url} isLazy={true} />;
-
-    //   return React.createElement(
-    //     'p',
-    //     propsWithUniqueKey({ className: [element.label || '', 'block-img'].join(' ') }, key),
-    //     linkUrl ? React.createElement('a', Object.assign({ href: linkUrl }, linkTarget, linkRel), img) : img,
-    //   );
-
-    // case Elements.embed: // Embed
-    //   props = Object.assign(
-    //     {
-    //       'data-oembed': element.oembed.embed_url,
-    //       'data-oembed-type': element.oembed.type,
-    //       'data-oembed-provider': element.oembed.provider_name,
-    //     },
-    //     element.label ? { className: element.label } : {},
-    //   );
-    //   const embedHtml = React.createElement('div', { dangerouslySetInnerHTML: { __html: element.oembed.html } });
-
-    //   return React.createElement('div', propsWithUniqueKey(props, key), embedHtml);
-
-    // case Elements.hyperlink: // Image
-    //   const targetAttr = element.data.target ? { target: element.data.target } : {};
-    //   const relAttr = element.data.target ? { rel: 'noopener' } : {};
-    //   props = Object.assign(
-    //     {
-    //       href: element.data.url || linkResolver(element.data),
-    //     },
-    //     targetAttr,
-    //     relAttr,
-    //   );
-
-    //   return React.createElement('a', propsWithUniqueKey(props, key), children);
-
-    // case Elements.label: // Label
-    //   props = element.data ? Object.assign({}, { className: element.data.label }) : {};
-
-    //   return React.createElement('span', propsWithUniqueKey(props, key), children);
-
-    // case Elements.span: // Span
-    //   if (content) {
-    //     return content.split('\n').reduce((acc, p) => {
-    //       if (acc.length === 0) {
-    //         return [p];
-    //       } else {
-    //         const brIndex = (acc.length + 1) / 2 - 1;
-    //         const br = React.createElement('br', propsWithUniqueKey({}, brIndex));
-
-    //         return acc.concat([br, p]);
-    //       }
-    //     }, []);
-    //   } else {
-    //     return null;
-    //   }
 
     default:
       // Always include a default that returns null

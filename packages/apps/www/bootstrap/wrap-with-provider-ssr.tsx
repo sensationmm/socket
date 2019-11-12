374a9681a40afa73331d3cb139dfef03647a1eeb
Tom Maton <thomas.maton@somoglobal.com>
2019-11-12 12:05:43 +0000

    fix(www): added translation provider wrapper to site to prevent reload
    fixes #1

--- packages/apps/www/bootstrap/wrap-with-provider-ssr.tsx
+++ packages/apps/www/bootstrap/wrap-with-provider-ssr.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
-
-import { Provider as Apollo } from '@somo/pda-graphql-apollo/src';
 import store from '../src/state/store';
 import apolloOptions from './apollo-options';
 
+import { Provider as Apollo } from '@somo/pda-graphql-apollo/src';
+import i18n from '@somo/pda-utils-i18n/src';
+
 export default ({ element }) => (
   <Apollo options={{ ssrMode: true, ...apolloOptions }}>
-    <Provider store={store}>{element}</Provider>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>{element}</Provider>
+    </I18nextProvider>
   </Apollo>
 );
